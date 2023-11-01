09df615bdf1106767d0f40feb653007872df6621
Aravind03 <gan.ara02@gmail.com>
2023-11-01 23:10:19 +0530

    Added candle before bday date and font change to bday date

--- src/webparts/birthdayWebPart/components/BirthdayWebPart.tsx
+++ src/webparts/birthdayWebPart/components/BirthdayWebPart.tsx
@@ -3,7 +3,6 @@ import styles from './BirthdayWebPart.module.scss';
 import type { IBirthdayWebPartProps } from './IBirthdayWebPartProps';
 import { Person } from '@microsoft/mgt-react/dist/es6/spfx';
 import { PersonCardInteraction, ViewType } from '@microsoft/mgt-spfx';
-//import { ViewType } from '@microsoft/mgt-spfx';
 import { MgtTemplateProps } from '@microsoft/mgt-react';
 export default class BirthdayWebPart extends React.Component<IBirthdayWebPartProps, {}> {
   public render(): React.ReactElement<IBirthdayWebPartProps> {
@@ -11,7 +10,7 @@ export default class BirthdayWebPart extends React.Component<IBirthdayWebPartPro
     return (
       <section className={`${styles.birthdayWebPart}`}>
         <>
-        <div><h4 style={{ backgroundColor : "powderblue" , width : "100%" ,fontSize: 20, textAlign : 'center'}} >Upcoming Birthdays</h4></div>
+        <div style={{ backgroundColor : "powderblue" , width : "100%" ,fontSize: 20, textAlign : 'center', fontFamily : 'var(--default-font-family, "Segoe UI", "Segoe UI Web (West European)", "Segoe UI", -apple-system, "BlinkMacSystemFont", "Roboto", "Helvetica Neue", sans-serif)'}} ><b>Upcoming Birthdays</b></div>
         </>
         {
           this.props.userInformation.map((user)=>
@@ -19,16 +18,25 @@ export default class BirthdayWebPart extends React.Component<IBirthdayWebPartPro
             const DisplayBirthDay = (props: MgtTemplateProps) => {
               return(
                 <>
-                  <div><b>{user.birthDate}</b></div>
+                <table>
+                  <tr>
+                    <td>
+                    <img alt="" src={require('./candle.png')} width={25} height={25}/>
+                    </td>
+                    <td>
+                    <div style={{color : 'black', paddingTop: -10}}>{user.birthDate}</div>
+                    </td>
+                  </tr>
+                </table>
                 </>
               )
             };
             return(
               <>
+                <br></br>
                 <Person personQuery={user.userPrincipalName} personCardInteraction={PersonCardInteraction.hover} view={ViewType.twolines} showPresence >
                   <DisplayBirthDay template='line2' />
                 </Person>
-                <br></br>
               </>
             );
           })
